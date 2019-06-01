993dbb589c3cfcf36f708a9b7ed7756e9a3298e9
anthhub <anthhub@163.com>
2019-06-01 22:12:22 +0800

    enhance function

--- src/func/index.ts
+++ src/func/index.ts
@@ -1,21 +1,25 @@
 /**
  * @description 链式调用,出错保护
  * @author anthhub
- * @date 2019-05-25
+ * @date 2019-06-01
  * @export
  * @template T
  * @param {T} target
- * @param {(target: T) => any} fn
+ * @param {(string | { (target: T): any })} fn
  * @param {*} [defaultValue=null]
  * @returns {*}
  */
-export function get<T>(target: T, fn: (target: T) => any, defaultValue: any = null): any {
+export function get<T extends { [propName: string]: any }>(target: T, fn: string | { (target: T): any }, defaultValue: any = null): any {
   const caller = target
-  if (typeof fn !== 'function') {
-    throw TypeError('The second property must be a function！')
-  }
+
   try {
-    return fn(caller)
+    if (typeof fn === 'string') {
+      return target[fn]
+    }
+    if (typeof fn === 'function') {
+      return fn(target)
+    }
+    throw TypeError('The second property must be a function or string！')
   } catch (error) {
     console.warn('=============tank-utils=============')
     console.warn(`In the function from properties :`, fn.toString())
@@ -25,10 +29,11 @@ export function get<T>(target: T, fn: (target: T) => any, defaultValue: any = nu
   }
 }
 
-/** 延时函数, 返回一个promise, 配合async/await使用更佳
- * @description
+/**
+ * @description 延时函数, 返回一个promise, 配合async/await使用更佳
  * @author anthhub
- * @date 2019-05-26
+ * @date 2019-06-01
+ * @export
  * @param {number} [time=0]
  * @returns
  */
@@ -47,13 +52,14 @@ export function wait(time: number = 0) {
 /**
  * @description 函数节流
  * @author anthhub
- * @date 2019-05-26
+ * @date 2019-06-01
+ * @export
  * @template T
  * @param {T} fn
- * @param {number} delay
+ * @param {number} [delay=0]
  * @returns {T}
  */
-export function throttle<T extends () => any>(fn: T, delay: number): T {
+export function throttle<T extends () => any>(fn: T, delay: number = 0): T {
   if (typeof fn !== 'function') {
     throw TypeError('The first property must be a function！')
   }
@@ -76,13 +82,14 @@ export function throttle<T extends () => any>(fn: T, delay: number): T {
 /**
  * @description 函数防抖
  * @author anthhub
- * @date 2019-05-26
+ * @date 2019-06-01
+ * @export
  * @template T
  * @param {T} fn
- * @param {number} delay
+ * @param {number} [delay=0]
  * @returns {T}
  */
-export function debounce<T extends () => any>(fn: T, delay: number): T {
+export function debounce<T extends () => any>(fn: T, delay: number = 0): T {
   if (typeof fn !== 'function') {
     throw TypeError('The first property must be a function！')
   }
