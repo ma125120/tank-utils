f8c6195943efbc567de6867642d3621a4a3fc934
anthhub <anthhub@163.com>
2019-06-04 14:58:30 +0800

    update export

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,10 @@ import * as func from './func/index'
 
 const tank = { ...date, ...func }
 
+export const get = func.get
+export const wait = func.wait
+export const debounce = func.debounce
+export const throttle = func.throttle
+export const timeToLocalStr = date.timeToLocalStr
+
 export default tank
