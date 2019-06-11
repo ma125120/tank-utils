9b7aea477ab40f1c493f8c187f3a0a023a4a561d
anthhub <anthhub@163.com>
2019-06-11 10:03:35 +0800

    添加pick方法

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as func from './func/index'
 const tank = { ...date, ...func }
 
 export const get = func.get
+export const pick = func.get
 export const wait = func.wait
 export const debounce = func.debounce
 export const throttle = func.throttle
