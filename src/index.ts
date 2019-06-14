85f35bfac90beef3486eb337b3c9487370e84321
anthhub <anthhub@163.com>
2019-06-14 11:04:05 +0800

    update export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import * as func from './func/index'
 
 const tank = { ...date, ...func }
 
+export const non = func.non
+export const compose = func.compose
 export const get = func.get
 export const pick = func.get
 export const wait = func.wait
